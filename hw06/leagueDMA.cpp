79ba9eb63ffb5478f075637fd26550af16bb4b26
ez <psizzlepip@yahoo.com>
2015-10-02 14:58:32 -0700

    updated with sample output

--- hw06/leagueDMA.cpp
+++ hw06/leagueDMA.cpp
@@ -111,3 +111,60 @@ void display(string n[], int s[], int size)
 		cout << n[i] << " wins: " << s[i] << endl;
 		
 }
+
+/* Sample Output
+
+[ezimmer2@hills hw06]$ a.out 
+How many teams are in the league? 3
+Enter Team #1's name: Bears
+Enter Team #1's number of wins: 1
+Enter Team #2's name: Lions
+Enter Team #2's number of wins: 13
+Enter Team #3's name: Tigers
+Enter Team #3's number of wins: 8
+League Standings
+Lions wins: 13
+Tigers wins: 8
+Bears wins: 1
+
+[ezimmer2@hills hw06]$ a.out 
+How many teams are in the league? 12
+Enter Team #1's name: Gumps
+Enter Team #1's number of wins: 18
+Enter Team #2's name: Zombies
+Enter Team #2's number of wins: 27
+Enter Team #3's name: Gravies
+Enter Team #3's number of wins: 31
+Enter Team #4's name: Strawberries
+Enter Team #4's number of wins: 15
+Enter Team #5's name: Peppers
+Enter Team #5's number of wins: 23
+Enter Team #6's name: Bowies
+Enter Team #6's number of wins: 72
+Enter Team #7's name: Mars
+Enter Team #7's number of wins: 50
+Enter Team #8's name: Salts
+Enter Team #8's number of wins: 23
+Enter Team #9's name: Lifers
+Enter Team #9's number of wins: 64
+Enter Team #10's name: Landlords
+Enter Team #10's number of wins: 3
+Enter Team #11's name: Cranes
+Enter Team #11's number of wins: 12
+Enter Team #12's name: Hacksaws
+Enter Team #12's number of wins: 44
+League Standings
+Bowies wins: 72
+Lifers wins: 64
+Mars wins: 50
+Hacksaws wins: 44
+Gravies wins: 31
+Zombies wins: 27
+Peppers wins: 23
+Salts wins: 23
+Gumps wins: 18
+Strawberries wins: 15
+Cranes wins: 12
+Landlords wins: 3
+[ezimmer2@hills hw06]$
+*/
