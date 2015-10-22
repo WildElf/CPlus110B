a74b8563c31b58e659e8afc869dd147acdd53e23
ez <psizzlepip@yahoo.com>
2015-10-21 22:46:22 -0700

    homework submitted

--- hw10/Circle.cpp
+++ hw10/Circle.cpp
@@ -7,16 +7,19 @@ using namespace std;
 
 const double PI = 3.14;
 
+// public set function X
 void Circle::setX(double value)
 {
 	x = value;
 }
 
+// public set function Y
 void Circle::setY(double value)
 {
 	y = value;
 }
 
+// public set function Radius
 void Circle::setRadius(double r)
 {
 	if (r >= 0)
@@ -25,26 +28,33 @@ void Circle::setRadius(double r)
 		radius = -1 * r;
 }
 
-double Circle::getRadius()
-{
-	return radius;
-}
-
+// get public value for X
 double Circle::getX()
 {
 	return x;
 }
 
+// get public value for X
 double Circle::getY()
 {
 	return y;
 }
 
+// get public value for Radius
+double Circle::getRadius()
+{
+	return radius;
+}
+
+// get the circle's area
 double Circle::getArea()
 {
 	return (radius * radius * PI);
 }
 
+// check the X, Y coordinates and return true if they are inside the circle
+// a point on the edge of the circle is considered inside
+// also display the distance from the point to the center (for validation and fun)
 bool Circle::containsPoint(double xValue, double yValue)
 {
 	double side1 = x - xValue;
--- hw10/CircleProgram.cpp
+++ hw10/CircleProgram.cpp
@@ -5,16 +5,13 @@
 
 using namespace std;
 
-void circleMaker(Circle *array, int number);
-
 int main ()
 {
 	// declare variables to create the circle
 	double circleX, circleY, circleRadius;
 	
-	int numCircle;
-	
-	cout << "First, create a circle.\n";
+	// create a circle defined by the user
+	cout << "\nFirst, create a circle.\n";
 	cout << "Enter the X center coordinate: ";
 	cin >> circleX;
 	
@@ -24,36 +21,69 @@ int main ()
 	cout << "Enter the radius: ";
 	cin >> circleRadius;
 	
+	cout << "The area of this circle should be " << circleRadius * circleRadius * 3.14 << endl;
+	
 	// build a circle object with entered values
 	Circle circle1;
 	
 	circle1.setX(circleX);
 	circle1.setY(circleY);
 	circle1.setRadius(circleRadius);
-	
-	cout << "The circle is made at " << circle1.getX() << " by " << circle1.getY() 
-		<< ", and a radius of " << circle1.getRadius() << endl;
+
+	cout << "The circle is made at (" << circle1.getX() << ", " << circle1.getY() 
+		<< "), and a radius of " << circle1.getRadius() << endl;
 	cout << "The area of the circle is " << circle1.getArea() << endl;
-		
-	// declare variables to get a point to test
+	
+	// set a circle pointer
+	Circle* circlePtr = &circle1;
+	
+	// modify circle values using the pointer
+	cout << "\nNow we'll change the circle values using pointers.\n";
+	cout << "Enter the new X coordinates: ";
+	cin >> circleX;	
+	circlePtr->setX(circleX);
+
+	cout << "Enter the new Y coordinate: ";
+	cin >> circleY;
+	circlePtr->setY(circleY);
+	
+	cout << "Enter the new radius: ";
+	cin >> circleRadius;
+	circlePtr->setRadius(circleRadius);
+
+	cout << "Using pointers, the circle is now at (" << circlePtr->getX() << ", " << circlePtr->getY() 
+		<< ") with a radius of " << circlePtr->getRadius() << endl;
+	cout << "The area of the circle is now " << circlePtr->getArea() << endl;
+			
+	// declare variables for point testing
 	double testX, testY;
+	bool checkPoint = true;
+	char testAgain;
 	
-	cout << "\nEnter the X coordinate to test: ";
-	cin >> testX;
+	while (checkPoint)
+	{
+		cout << "\nEnter the X coordinate to test: ";
+		cin >> testX;
 	
-	cout << "Enter the Y coordinate to test: ";
-	cin >> testY;
+		cout << "Enter the Y coordinate to test: ";
+		cin >> testY;
 	
-	// check the point using the Circle class method
-	bool insidePoint = circle1.containsPoint(testX, testY);
+		// check the point using the Circle class method
+		bool insidePoint = circlePtr->containsPoint(testX, testY);
 	
-	if (insidePoint)
-		cout << "The point is inside the circle.\n";
-	else
-		cout << "The point is outside the circle.\n";
+		if (insidePoint)
+			cout << "The point is inside the circle.\n";
+		else
+			cout << "The point is outside the circle.\n";
+			
+		cout << "Test another point? (y/n) ";
+		cin >> testAgain;
+		
+		if (tolower(testAgain) == 'n')
+			checkPoint = false;
+		else
+			cout << "Checking another point...";
+	}
 }
 
-void circleMaker(Circle *array, int number)
-{
 
-}
