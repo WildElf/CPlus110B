8f68f14d4433ebfd4302f04919ce6afc5c93c927
ez <psizzlepip@yahoo.com>
2015-10-22 22:36:25 -0700

    variable number of circles, repeating choice menu

--- hw10/CircleProgram.cpp
+++ hw10/CircleProgram.cpp
@@ -5,85 +5,185 @@
 
 using namespace std;
 
+int menu();
+void changeCircleValues(Circle,int);
+
 int main ()
 {
-	// declare variables to create the circle
-	double circleX, circleY, circleRadius;
-	
-	// create a circle defined by the user
-	cout << "\nFirst, create a circle.\n";
-	cout << "Enter the X center coordinate: ";
-	cin >> circleX;
-	
-	cout << "Enter the Y center coordinate: ";
-	cin >> circleY;
-	
-	cout << "Enter the radius: ";
-	cin >> circleRadius;
-	
-	cout << "The area of this circle should be " << circleRadius * circleRadius * 3.14 << endl;
-	
-	// build a circle object with entered values
-	Circle circle1;
-	
-	circle1.setX(circleX);
-	circle1.setY(circleY);
-	circle1.setRadius(circleRadius);
-
-	cout << "The circle is made at (" << circle1.getX() << ", " << circle1.getY() 
-		<< "), and a radius of " << circle1.getRadius() << endl;
-	cout << "The area of the circle is " << circle1.getArea() << endl;
-	
-	// set a circle pointer
-	Circle* circlePtr = &circle1;
-	
-	// modify circle values using the pointer
-	cout << "\nNow we'll change the circle values using pointers.\n";
-	cout << "Enter the new X coordinates: ";
-	cin >> circleX;	
-	circlePtr->setX(circleX);
-
-	cout << "Enter the new Y coordinate: ";
-	cin >> circleY;
-	circlePtr->setY(circleY);
-	
-	cout << "Enter the new radius: ";
-	cin >> circleRadius;
-	circlePtr->setRadius(circleRadius);
-
-	cout << "Using pointers, the circle is now at (" << circlePtr->getX() << ", " << circlePtr->getY() 
-		<< ") with a radius of " << circlePtr->getRadius() << endl;
-	cout << "The area of the circle is now " << circlePtr->getArea() << endl;
-			
-	// declare variables for point testing
-	double testX, testY;
-	bool checkPoint = true;
-	char testAgain;
-	
-	while (checkPoint)
-	{
-		cout << "\nEnter the X coordinate to test: ";
-		cin >> testX;
-	
-		cout << "Enter the Y coordinate to test: ";
-		cin >> testY;
-	
-		// check the point using the Circle class method
-		bool insidePoint = circlePtr->containsPoint(testX, testY);
-	
-		if (insidePoint)
-			cout << "The point is inside the circle.\n";
-		else
-			cout << "The point is outside the circle.\n";
-			
-		cout << "Test another point? (y/n) ";
-		cin >> testAgain;
+	// variables for user controlled number of circles
+	int numCircles;
+	Circle *circleCollection = NULL;
 		
-		if (tolower(testAgain) == 'n')
-			checkPoint = false;
-		else
-			cout << "Checking another point...";
+	// declare variables to create circles
+	double circleX, circleY, circleRadius;
+
+	cout << "How many circles do you want to make? ";
+	cin >> numCircles;
+	
+	if (numCircles < 1)
+	{
+		cout << "Let's make one anyway.\n";
+		numCircles = 1;
 	}
+	
+	// create an array to allow a user defined number of circles
+	circleCollection = new Circle[numCircles];
+	
+	cout << "Now to make " << numCircles << " circles.\n";
+
+	// loop for all circles in the array
+	for (int i = 0; i < numCircles; i++)
+	{
+		cout << "Enter the X coordinate for Circle #" << i + 1 << ": ";
+		cin >> circleX;
+	
+		cout << "Enter the Y coordinate for Circle #" << i + 1 << ": ";
+		cin >> circleY;
+		
+		cout << "Enter the radius for Circle #" << i + 1 << ": ";
+		cin >> circleRadius;
+
+		cout << "The area of this circle should be " << circleRadius * circleRadius * 3.14 << endl;
+		
+		if (i == 12)
+			cout << "This sure is a lot of circles.\n";
+		
+		// build a circle object with entered values
+		// Circle circleCollection[i];
+	
+		circleCollection[i].setX(circleX);
+		circleCollection[i].setY(circleY);
+		circleCollection[i].setRadius(circleRadius);
+
+	}
+	
+	// display all circles created
+	cout << "Now there are " << numCircles << " circles.\n";
+	for (int i = 0; i < numCircles; i++)
+	{
+		cout << "Circle #" << i + 1 << " is at (" << circleCollection[i].getX() << ", " << circleCollection[i].getY()
+			<< ") with a radius of " << circleCollection[i].getRadius() 
+			<< " and an area of " << circleCollection[i].getArea() << endl;
+	}
+	
+		
+	// set up do-while logic for repeatable portion
+	bool loop = true;
+
+	do
+	{
+		// run the menu function
+		int choice = menu();
+		
+		if (choice == 1)
+		{
+			// let's user select one or many circles to change
+			int changeNum;
+
+			// modify circle values using the pointer
+			cout << "\nNow we'll change circle values using pointers.\n";
+			cout << "Which circle(s) would you like to change? (enter 0 to change all): ";
+			cin >> changeNum;
+
+			// TODO update to use pointer
+			if (changeNum < 0 || changeNum > numCircles) // basic bounds checking
+			{
+				cout << "That's confusing me, so we'll change them all!";
+				changeNum = 0;
+			}
+			else if (changeNum == 0)
+				for (int i = 0; i < numCircles; i++) // loop for each circle
+				{
+					cout << "Enter the X coordinate for Circle #" << i + 1 << ": ";
+					cin >> circleX;
+
+					cout << "Enter the Y coordinate for Circle #" << i + 1 << ": ";
+					cin >> circleY;
+
+					cout << "Enter the radius for Circle #" << i + 1 << ": ";
+					cin >> circleRadius;
+
+					// update circle
+					(circleCollection+i)->setX(circleX);
+					(circleCollection+i)->setY(circleY);
+					(circleCollection+i)->setRadius(circleRadius);
+
+				}
+			else // use changeNum to change only selected circle
+			{
+				cout << "Enter the X coordinate for Circle #" << changeNum << ": ";
+				cin >> circleX;
+	
+				cout << "Enter the Y coordinate for Circle #" << changeNum << ": ";
+				cin >> circleY;
+		
+				cout << "Enter the radius for Circle #" << changeNum << ": ";
+				cin >> circleRadius;
+				
+				// update the circle, accounting for index shift
+				(circleCollection+(changeNum-1))->setX(circleX);
+				(circleCollection+(changeNum-1))->setY(circleY);
+				(circleCollection+(changeNum-1))->setRadius(circleRadius);
+			}
+			
+			// redisplay circles		
+			for (int i = 0; i < numCircles; i++)
+			{
+				cout << "Circle #" << i + 1 << " is at (" << circleCollection[i].getX() << ", " << circleCollection[i].getY()
+					<< ") with a radius of " << circleCollection[i].getRadius() 
+					<< " and an area of " << circleCollection[i].getArea() << endl;
+			}
+
+		}
+		else if (choice == 2)
+		{
+			double testX, testY;
+			bool insidePoint = false;
+
+			// get points to test
+			cout << "\nEnter the X coordinate to test: ";
+			cin >> testX;
+			
+			cout << "Enter the Y coordinate to test: ";
+			cin >> testY;
+
+			for (int i = 0; i < numCircles; i++)
+			{
+				// check the point using the Circle class method
+				insidePoint = circleCollection[i].containsPoint(testX, testY);
+			
+				if (insidePoint)
+					cout << "The point is inside circle #" << i + 1 << ".\n";
+				else
+					cout << "The point is outside circle #" << i + 1 << ".\n";
+
+			}
+		}	
+		else  // terminate the loop
+		{
+			cout << "The circle is broken. Goodbye!\n";
+			loop = false;
+		}
+		
+	} while (loop);
 }
 
+int menu()
+{
+	int menuChoice;
+	do
+	{
+		cout << "What now?\n";
+		cout << "1. Change circle values (with pointers)\n";
+		cout << "2. Find out if a point exists in any circles\n";
+		cout << "3. Quit\n";
+		cout << "Select 1-3: ";
+		cin >> menuChoice;
+		
+		if (menuChoice < 1 || menuChoice > 3)
+			cout << "Invalid selection.\n\n";
 
+	} while (menuChoice < 1 || menuChoice > 3);
+	
+	return menuChoice;
+}
